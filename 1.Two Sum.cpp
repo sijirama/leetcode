45be6bd1b284988220d5b313f9f2c2d00a3889f3
sijibomi <gbemilesanmi@gmail.com>
2023-07-16 13:57:21 +0100

    formatted some of em

--- 1.Two Sum.cpp
+++ 1.Two Sum.cpp
@@ -1,45 +1,45 @@
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-You can return the answer in any order.
-Example 1:
-Input: nums = [2,7,11,15], target = 9
+Given an array of integers nums and an integer target, return indices of the two
+numbers such that they add up to target. You may assume that each input would
+have exactly one solution, and you may not use the same element twice. You can
+return the answer in any order. Example 1: Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
 
 #include <algorithm>
+#include <iostream>
 #include <iterator>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target);
+vector<int> twoSum(vector<int> &nums, int target);
 
-int main(){
-    vector <int> Answer = {3,2,4};
-    int target = 6;
-    auto ans = twoSum(Answer , target);
-    for(auto i : ans){
-        cout << i << ", ";
-    }
+int main() {
+  vector<int> Answer = {3, 2, 4};
+  int target = 6;
+  auto ans = twoSum(Answer, target);
+  for (auto i : ans) {
+    cout << i << ", ";
+  }
 }
 
-vector<int> twoSum(vector<int>& nums, int target) {
-    vector <int> Answer;
+vector<int> twoSum(vector<int> &nums, int target) {
+  vector<int> Answer;
 
-    for(int i = 0 ; i < nums.size() ; ++i){
-        int num1 = nums[i];
+  for (int i = 0; i < nums.size(); ++i) {
+    int num1 = nums[i];
 
-        for(int j = 0 ; j < nums.size() ; ++j){
-            if(j == i ) continue;
+    for (int j = 0; j < nums.size(); ++j) {
+      if (j == i)
+        continue;
 
-            if (nums[i] + nums[j] == target ){
-                return vector <int> {i , j};
-            }
-        }
+      if (nums[i] + nums[j] == target) {
+        return vector<int>{i, j};
+      }
     }
+  }
 
-    return Answer;
+  return Answer;
 }
-
--- 118.Pascals Triangle.cpp
+++ 118.Pascals Triangle.cpp
@@ -3,43 +3,39 @@
 Given an integer array nums of unique elements, return all possible
 subsets
 (the power set).
-The solution set must not contain duplicate subsets. Return the solution in any order.
-Example 1:
-Input: nums = [1,2,3]
-Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+The solution set must not contain duplicate subsets. Return the solution in any
+order. Example 1: Input: nums = [1,2,3] Output:
+[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 */
 
-
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
-vector<int> pascals(int num){
-    vector<int> Result;
+vector<int> pascals(int num) {
+  vector<int> Result;
 
-    for(int i = 0 ; i < num ; ++i){
+  for (int i = 0; i < num; ++i) {
+  }
 
-
-    }
-
-    return Result;
+  return Result;
 }
 
 vector<vector<int>> generate(int numRows) {
-    vector<vector<int>> Result;
+  vector<vector<int>> Result;
 
-    return Result;
+  return Result;
 }
 
+int main() {
 
-int main(){
-    
-   vector<vector<int>> result = generate(5) ;
-    // for(vector<int> i : result){
-    //    for(int j : i){
-    //        cout << j << " ";
-    //    }
-    //    cout << endl;
-    // }
+  vector<vector<int>> result = generate(5);
+  // for(vector<int> i : result){
+  //    for(int j : i){
+  //        cout << j << " ";
+  //    }
+  //    cout << endl;
+  // }
 }
